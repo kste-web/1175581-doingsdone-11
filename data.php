4cd7dc26ee1952cd669c9a2d0f7dfb5b1ae9aa4d
Карен <kste.web@gmail.com>
2019-11-13 20:38:21 +0300

    module3-task1
    также переделал циклы while на foreach

---
+++ data.php
@@ -0,0 +1,44 @@
+<?php
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+$projects = ['Входящие','Учеба','Работа','Домашние дела','Авто'];
+$projects_count = count($projects);
+$tasks = [
+	[
+		'task' => 'Собеседование в IT компании',
+		'date' => '01.12.2019',
+		'category' => 'Работа',
+		'is_complete' => false
+	],
+	[
+		'task' => 'Выполнить тестовое задание',
+		'date' => '25.12.2019',
+		'category' => 'Работа',
+		'is_complete' => false
+	],
+	[
+		'task' => 'Сделать задание первого раздела',
+		'date' => '	21.12.2019',
+		'category' => 'Учеба',
+		'is_complete' => true
+	],
+	[
+		'task' => 'Встреча с другом',
+		'date' => '22.12.2019',
+		'category' => 'Входящие',
+		'is_complete' => false
+	],
+	[
+		'task' => 'Купить корм для кота',
+		'date' => null,
+		'category' => 'Домашние дела',
+		'is_complete' => false
+	],
+	[
+		'task' => 'Заказать пиццу',
+		'date' => null,
+		'category' => 'Домашние дела',
+		'is_complete' => false
+	]
+];
+$tasks_count = count($tasks);
---
+++ templates/task.php
@@ -0,0 +1,13 @@
+<tr class="tasks__item task 
+<?php if($tasks[$index]['is_complete'] == true):?>
+	<?php echo 'task--completed'; ?>
+	<?php endif;?>">
+	<td class="task__select">
+		<label class="checkbox task__checkbox">
+			<input class="checkbox__input visually-hidden" type="checkbox" checked>
+			<span class="checkbox__text"><?=$task['task'];?></span>
+		</label>
+	</td>
+	<td class="task__date"><?=$task['date'];?></td>
+	<td class="task__controls"></td>
+</tr>
