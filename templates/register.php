7c673231dea3765200afdd73867450e7294846a5
kste-web <54847495+kste-web@users.noreply.github.com>
2019-12-19 04:17:22 +0300

    module7-task2

--- templates/register.php
+++ templates/register.php
@@ -19,7 +19,7 @@
         </a>
 
         <div class="main-header__side">
-          <a class="main-header__side-item button button--transparent" href="form-authorization.html">Войти</a>
+          <a class="main-header__side-item button button--transparent" href="enter.php">Войти</a>
         </div>
       </header>
 
